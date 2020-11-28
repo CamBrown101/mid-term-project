ee15f5979329deac7bdee0b487f5ca22d8070911
marcellmueller <mlmueller@protonmail.com>
2020-11-28 09:39:33 -0800

    Fixed temporary listings code, added login form

--- public/scripts/listingLoadByCategory.js
+++ public/scripts/listingLoadByCategory.js
@@ -58,10 +58,15 @@ const createNewCard = function (listing) {
 $(document).ready(() => {
   console.log("Document ready");
   const renderListing = (listings) => {
+    $("#listings-row-0").empty();
     $("#listings-row-1").empty();
-    listings.forEach((element) => {
-      $("#listings-row-1").append(createNewCard(element));
-    });
+    $("#listings-row-2").empty();
+
+    for (let i = 0; i < 4; i++) {
+      $("#listings-row-0").append(createNewCard(listings[i]));
+      $("#listings-row-1").append(createNewCard(listings[i]));
+      $("#listings-row-2").append(createNewCard(listings[i]));
+    }
   };
   const loadListings = () => {
     return $.ajax("/listings").then((listing) => {
