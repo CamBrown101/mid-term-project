ac5e79efcb0b8b7bf0735742c8df7d7f8cba244b
marcellmueller <mlmueller@protonmail.com>
2020-11-27 18:45:20 -0800

    moved renderListing

--- public/scripts/listingLoadByCategory.js
+++ public/scripts/listingLoadByCategory.js
@@ -1,16 +1,24 @@
 //Loads the initial listings on the home page by category
+const renderListing = function (listing) {
+  const articleContainer = $(`<article class="card-container">
+    <h5 class="card-title">${listing.title}</h5>
+    <img src="${listing.photo_url}">
+    <p class="card-description">${listing.description}</p>
+    <a src="#" class="btn btn-primary">Do Something</a>
+    </article>`);
+  return articleContainer;
+};
 
 $(document).ready(() => {
-const renderListings = (listings) => {
-  $().empty();
-  listings.forEach((element) => {
-    id.append(createNewCard(listings))
-  })
-}
-const loadListings = () => {
-  return $.ajax('/listings', { method: 'GET'})
-  .then((listings) => {
-    renderListings(listings)
-  })
-}
-})
+  const renderListings = (listings) => {
+    $().empty();
+    listings.forEach((element) => {
+      id.append(createNewCard(listings));
+    });
+  };
+  const loadListings = () => {
+    return $.ajax("/listings", { method: "GET" }).then((listings) => {
+      renderListing(listings);
+    });
+  };
+});
