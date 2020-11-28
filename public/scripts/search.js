480aca79f4b3ced4c94825168f9b62ebcc0df24e
cambrown101 <cameron-brown@live.com>
2020-11-28 10:14:19 -0800

    Display serach results correctly. Stopped empty search inputs.

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,29 +1,40 @@
 $(Document).ready(() => {
 
   const createNewCard = function (listing) {
-    const articleContainer = $(`<article class="card-container">
-      <h5 class="card-title">${listing.title}</h5>
-      <img src="${listing.photo_url}">
-      <p class="card-description">${listing.description}</p>
-      <a src="#" class="btn btn-primary">Do Something</a>
-      </article>`);
+    const articleContainer = $(`
+      <article class="card-container">
+        <h5 class="card-title">${listing.title}</h5>
+        <img src="${listing.photo_url}">
+        <p class="card-description">${listing.description}</p>
+        <a src="#" class="btn btn-primary">Do Something</a>
+      </article>
+      `);
     return articleContainer;
   };
+
+    const resultsContainerHtml = $(`<div class="results-container"></div>`)
+    const $mainContainer = $('.main-container');
+    
     const renderListing = (listings) => {
-      $('.main-container').empty();
+      $mainContainer.empty();
+      $mainContainer.append(resultsContainerHtml);
       listings.forEach((element) => {
-        $('.main-container').append(createNewCard(element));
+        const $resultsContainer = $('.results-container');
+        $resultsContainer.append(createNewCard(element));
       });
     };
   
     $('#item-search').submit((event)=>{
       const searchString = $('#text').val()
-      event.preventDefault();
-      $('.main-container').empty()
-       $.get(`/listings/?text=${searchString}`, (listing => {
-        console.log(listing)
-        renderListing(listing);
-      }))
+
+      if(searchString.trim()) {
+        event.preventDefault();
+        $('.results-container').empty();
+         $.get(`/listings/?text=${searchString}`, (listing => {
+          console.log(listing)
+          renderListing(listing);
+        }))
+      }
     })
     
     // $('#item-search').submit((event)=>{
