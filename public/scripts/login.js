e90ea698d8eb12e84d07535bd4947f1cd537d378
cambrown101 <cameron-brown@live.com>
2020-11-28 21:20:02 -0800

    Restore login.js

---
+++ public/scripts/login.js
@@ -0,0 +1,24 @@
+$(document).ready(() => {
+  $.get("/login", (data) => {
+    if (data) {
+      const username = data.name;
+      $("#login").hide();
+      $("#logout").show();
+      $(".usernameLoggedIn").text(`Logged in as ${username}`);
+    } else {
+      $("#login").show();
+      $("#logout").hide();
+    }
+  });
+  $(".login-btn").submit((event) => {
+    event.preventDefault();
+  });
+  $("#logout-btn").click(() => {
+    $.get("/logout", () => {
+      console.log("Logged Out");
+      $(".main-container").empty();
+      createCategoryRows();
+      homePageLoad();
+    });
+  });
+});
