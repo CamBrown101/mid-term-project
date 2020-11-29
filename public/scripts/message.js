832d6a419e427986d64f16e8dd1c9bc8c31966e6
marcellmueller <mlmueller@protonmail.com>
2020-11-29 12:26:31 -0800

    New message post listener half way implemented

--- public/scripts/message.js
+++ public/scripts/message.js
@@ -1,10 +1,24 @@
 $(document).ready(() => {
+  let listingId = 1;
   $("main").on("click", "#message-seller-btn", (event) => {
     event.preventDefault();
-    const listingId = $("#message-seller-btn").children("p").text();
+    listingId = $("#message-seller-btn").children("p").text();
     console.log(listingId);
     $.get(`/messages/${listingId}`, (listing) => {
       console.log(listing);
+    });
+  });
+
+  $("message-submit").submit((event) => {
+    event.preventDefault();
+    console.log("new-message-click-working");
+    const message = $(".message-input").val();
+    const data = {
+      message: message,
+    };
+
+    $.post(`/listings/${listingId}`, data, (message) => {
+      console.log(message);
     });
   });
 });
