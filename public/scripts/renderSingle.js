d405c0f02fa514e33da4f085ef0da8b20667e7f0
cambrown101 <cameron-brown@live.com>
2020-11-29 13:25:14 -0800

    render messages from single listing page

--- public/scripts/renderSingle.js
+++ public/scripts/renderSingle.js
@@ -22,6 +22,7 @@ $(document).ready(() => {
     });
     console.log("fave");
   });
+
   const createListingBig = function (listing) {
     const articleContainer = $(`
       <article class="big-listing">
