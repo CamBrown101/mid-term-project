3a80d5d5f3e296bde2df1ca98de874f7900c8a70
marcellmueller <mlmueller@protonmail.com>
2020-11-29 18:44:28 -0800

    Added click handler for conversations screen, traversed dom for hidden listingId and set up get request

--- public/scripts/conversations.js
+++ public/scripts/conversations.js
@@ -34,13 +34,20 @@ $(document).ready(() => {
     $.get(`/messages/`, (data) => {
       const conversations = data.messages;
       const id = data.id;
-      console.log(conversations);
       $(".main-container").empty();
       $(".main-container").append(createConversationContainer());
       for (const item of conversations) {
         $(".conversations").append(createConversations(item));
         $(".conversation-listing-id").hide();
       }
+    });
+  });
+
+  $("main").on("click", ".conversation", (event) => {
+    const listingId = $(".conversation:last-child").children().last().text();
+    console.log(listingId);
+    $.get(`/messages/${listingId}`, (data) => {
+      console.log(data);
     });
   });
 });
