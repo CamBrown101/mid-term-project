5e8a004abc47c50d623790dff41524f31bad8277
cambrown101 <cameron-brown@live.com>
2020-11-30 20:38:27 -0800

    Work on new message alert

--- public/scripts/newMessageAlerts.js
+++ public/scripts/newMessageAlerts.js
@@ -1,10 +1,17 @@
 $(document).ready(() => {
+  let currentCount = 0;
+
   const getMessageCount = () => {
     const getCount = $.get(`messages/count`, (count) => {
-      const newCount = count.rows[0];
+      let newCount = count.rows[0].count;
+      if (newCount > currentCount) {
+      }
+
+      let newCount = count.rows[0].count;
       console.log(newCount, "inside");
-      return newCount;
     });
     return getCount;
   };
+
+  getMessageCount();
 });
