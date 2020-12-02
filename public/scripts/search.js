09884b4655c586923cc12093ed15b88706eaa380
Michael Krishna <michaelkrishna@hotmail.com>
2020-12-02 01:14:30 +0000

    Commenting code and general cleanup

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,20 +1,15 @@
 $(document).ready(() => {
-  // //jQuery variables
-  // const $searchBar = $(".search-bar");
-  // //Show button on click
-  // $(".hamburger").click(() => {
-  //   $searchBar.fadeToggle("slow");
-  // });
 
+  //Render all listing objects in given array
   const renderListing = (listings) => {
     $(".main-container").empty();
     $(".main-container").append(`<div class="results-container"></div>`);
+    //Loops through results and adds new cards
     listings.forEach((element) => {
       $(".results-container").append(createNewCard(element));
       $(".id").hide();
     });
   };
-  //Loops through results and adds new cards
 
   //Takes search input and sends to server to query results from database
   $("#item-search").submit((event) => {
@@ -29,22 +24,17 @@ $(document).ready(() => {
       clearResultsContainer();
       $.get("/listings", data, (listing) => {
         renderListing(listing);
-        $searchBar.fadeToggle("slow", () => {
-          $searchBar.hide();
-        });
       });
     }
   });
 
+  //Takes user to page displaying their favourited listings
   $("#get-faves-button").click((event) => {
     event.preventDefault();
     clearResultsContainer();
     $.get("/listings/favourites", (listing) => {
       $(".results-container").remove()
       renderListing(listing);
-      // $searchBar.fadeToggle("slow", () => {
-      //   $searchBar.hide();
-      // });
     });
   });
 });
