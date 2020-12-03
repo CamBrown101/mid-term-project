8eab31c18c7cc69af89db547d8a180cb8be2bdd3
Michael Krishna <michaelkrishna@hotmail.com>
2020-12-03 00:53:00 +0000

    Functionality of search filter

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -3,6 +3,16 @@ $(document).ready(() => {
   const renderListing = (listings) => {
     $(".main-container").empty();
     $(".main-container").append(`<div class="results-container"></div>`);
+    $(".results-container").append(`
+    <div class="filter-options">
+      <form class="filter">
+        <label for="price-max">Max:</label>
+        <input type="number" id="price-max" required name="max"/>
+        <label for="price-max">Min:</label>
+        <input type="number" id="price-min" required name="min"/>
+        <button type="submit" class="login-btn btn btn-primary">Filter</button>
+      </form>
+    </div>`);
     //Loops through results and adds new cards
     listings.forEach((element) => {
       $(".results-container").append(createNewCard(element));
@@ -26,6 +36,17 @@ $(document).ready(() => {
       });
     }
   });
+
+  $("main").on("submit", ".filter", (event) => {
+    event.preventDefault();
+    const searchString = $("#search-input").val();
+    const maxPrice = $("#price-max").val();
+    const minPrice = $("#price-min").val();
+    $.get("/listings", {text: searchString}, (listings) => {
+      const filteredListings = listings.filter(listing => listing.price >= minPrice && listing.price <= maxPrice);
+      renderListing(filteredListings);
+    });
+  })
 
   //Takes user to page displaying their favourited listings
   $("#get-faves-button, #favourites-title").click((event) => {
